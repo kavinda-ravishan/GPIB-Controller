ce40752f615e6ca926a552f587d619176aaa51ab
kavinda ravishan <kvnravishan@outlook.com>
2020-07-31 19:53:34 +0530

    2020-02-07

---
+++ PolarizationAnalyzer/Form1.Designer.cs
@@ -0,0 +1,291 @@
+﻿using NationalInstruments.NI4882;
+
+namespace PolarizationAnalyzer
+{
+    partial class MainForm
+    {
+        private Device device;
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (device != null)
+                {
+                    device.Dispose();
+                }
+                if (components != null)
+                {
+                    components.Dispose();
+                }
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.boardIdLabel = new System.Windows.Forms.Label();
+            this.primaryAddressLabel = new System.Windows.Forms.Label();
+            this.secondaryAddressLabel = new System.Windows.Forms.Label();
+            this.secondaryAddressComboBox = new System.Windows.Forms.ComboBox();
+            this.primaryAddressNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.boardIdNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.closeButton = new System.Windows.Forms.Button();
+            this.openButton = new System.Windows.Forms.Button();
+            this.stringReadLabel = new System.Windows.Forms.Label();
+            this.readButton = new System.Windows.Forms.Button();
+            this.stringToWriteLabel = new System.Windows.Forms.Label();
+            this.writeButton = new System.Windows.Forms.Button();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.stringReadTextBox = new System.Windows.Forms.RichTextBox();
+            this.stringToWriteTextBox = new System.Windows.Forms.TextBox();
+            this.lblEyx = new System.Windows.Forms.Label();
+            this.lblDelta = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.primaryAddressNumericUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.boardIdNumericUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // boardIdLabel
+            // 
+            this.boardIdLabel.AutoSize = true;
+            this.boardIdLabel.Location = new System.Drawing.Point(69, 15);
+            this.boardIdLabel.Name = "boardIdLabel";
+            this.boardIdLabel.Size = new System.Drawing.Size(52, 13);
+            this.boardIdLabel.TabIndex = 0;
+            this.boardIdLabel.Text = "Board ID:";
+            // 
+            // primaryAddressLabel
+            // 
+            this.primaryAddressLabel.AutoSize = true;
+            this.primaryAddressLabel.Location = new System.Drawing.Point(36, 41);
+            this.primaryAddressLabel.Name = "primaryAddressLabel";
+            this.primaryAddressLabel.Size = new System.Drawing.Size(85, 13);
+            this.primaryAddressLabel.TabIndex = 2;
+            this.primaryAddressLabel.Text = "Primary Address:";
+            // 
+            // secondaryAddressLabel
+            // 
+            this.secondaryAddressLabel.AutoSize = true;
+            this.secondaryAddressLabel.Location = new System.Drawing.Point(19, 68);
+            this.secondaryAddressLabel.Name = "secondaryAddressLabel";
+            this.secondaryAddressLabel.Size = new System.Drawing.Size(102, 13);
+            this.secondaryAddressLabel.TabIndex = 4;
+            this.secondaryAddressLabel.Text = "Secondary Address:";
+            // 
+            // secondaryAddressComboBox
+            // 
+            this.secondaryAddressComboBox.Location = new System.Drawing.Point(127, 65);
+            this.secondaryAddressComboBox.Name = "secondaryAddressComboBox";
+            this.secondaryAddressComboBox.Size = new System.Drawing.Size(56, 21);
+            this.secondaryAddressComboBox.TabIndex = 5;
+            // 
+            // primaryAddressNumericUpDown
+            // 
+            this.primaryAddressNumericUpDown.Location = new System.Drawing.Point(127, 39);
+            this.primaryAddressNumericUpDown.Name = "primaryAddressNumericUpDown";
+            this.primaryAddressNumericUpDown.Size = new System.Drawing.Size(40, 20);
+            this.primaryAddressNumericUpDown.TabIndex = 3;
+            this.primaryAddressNumericUpDown.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            // 
+            // boardIdNumericUpDown
+            // 
+            this.boardIdNumericUpDown.Location = new System.Drawing.Point(127, 13);
+            this.boardIdNumericUpDown.Name = "boardIdNumericUpDown";
+            this.boardIdNumericUpDown.Size = new System.Drawing.Size(40, 20);
+            this.boardIdNumericUpDown.TabIndex = 1;
+            // 
+            // closeButton
+            // 
+            this.closeButton.Location = new System.Drawing.Point(102, 110);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(75, 23);
+            this.closeButton.TabIndex = 7;
+            this.closeButton.Text = "&Close";
+            this.closeButton.Click += new System.EventHandler(this.CloseButton_Click);
+            // 
+            // openButton
+            // 
+            this.openButton.Location = new System.Drawing.Point(22, 110);
+            this.openButton.Name = "openButton";
+            this.openButton.Size = new System.Drawing.Size(75, 23);
+            this.openButton.TabIndex = 6;
+            this.openButton.Text = "&Open";
+            this.openButton.Click += new System.EventHandler(this.OpenButton_Click);
+            // 
+            // stringReadLabel
+            // 
+            this.stringReadLabel.Location = new System.Drawing.Point(204, 5);
+            this.stringReadLabel.Name = "stringReadLabel";
+            this.stringReadLabel.Size = new System.Drawing.Size(75, 23);
+            this.stringReadLabel.TabIndex = 16;
+            this.stringReadLabel.Text = "String Read:";
+            // 
+            // readButton
+            // 
+            this.readButton.Location = new System.Drawing.Point(127, 215);
+            this.readButton.Name = "readButton";
+            this.readButton.Size = new System.Drawing.Size(75, 23);
+            this.readButton.TabIndex = 15;
+            this.readButton.Text = "&Read";
+            this.readButton.Click += new System.EventHandler(this.ReadButton_Click);
+            // 
+            // stringToWriteLabel
+            // 
+            this.stringToWriteLabel.Location = new System.Drawing.Point(22, 160);
+            this.stringToWriteLabel.Name = "stringToWriteLabel";
+            this.stringToWriteLabel.Size = new System.Drawing.Size(100, 23);
+            this.stringToWriteLabel.TabIndex = 14;
+            this.stringToWriteLabel.Text = "String to Write:";
+            // 
+            // writeButton
+            // 
+            this.writeButton.Location = new System.Drawing.Point(27, 215);
+            this.writeButton.Name = "writeButton";
+            this.writeButton.Size = new System.Drawing.Size(75, 23);
+            this.writeButton.TabIndex = 13;
+            this.writeButton.Text = "&Write";
+            this.writeButton.Click += new System.EventHandler(this.WriteButton_Click);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(27, 270);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(202, 202);
+            this.pictureBox.TabIndex = 17;
+            this.pictureBox.TabStop = false;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(235, 298);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(301, 45);
+            this.trackBar1.TabIndex = 19;
+            this.trackBar1.Scroll += new System.EventHandler(this.TrackBar1_Scroll);
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(235, 404);
+            this.trackBar2.Maximum = 100;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(301, 45);
+            this.trackBar2.TabIndex = 20;
+            this.trackBar2.Scroll += new System.EventHandler(this.TrackBar2_Scroll);
+            // 
+            // stringReadTextBox
+            // 
+            this.stringReadTextBox.Location = new System.Drawing.Point(207, 31);
+            this.stringReadTextBox.Name = "stringReadTextBox";
+            this.stringReadTextBox.ReadOnly = true;
+            this.stringReadTextBox.Size = new System.Drawing.Size(329, 152);
+            this.stringReadTextBox.TabIndex = 21;
+            this.stringReadTextBox.Text = "";
+            // 
+            // stringToWriteTextBox
+            // 
+            this.stringToWriteTextBox.Location = new System.Drawing.Point(25, 189);
+            this.stringToWriteTextBox.Name = "stringToWriteTextBox";
+            this.stringToWriteTextBox.Size = new System.Drawing.Size(511, 20);
+            this.stringToWriteTextBox.TabIndex = 22;
+            // 
+            // lblEyx
+            // 
+            this.lblEyx.AutoSize = true;
+            this.lblEyx.Location = new System.Drawing.Point(247, 271);
+            this.lblEyx.Name = "lblEyx";
+            this.lblEyx.Size = new System.Drawing.Size(13, 13);
+            this.lblEyx.TabIndex = 23;
+            this.lblEyx.Text = "--";
+            // 
+            // lblDelta
+            // 
+            this.lblDelta.AutoSize = true;
+            this.lblDelta.Location = new System.Drawing.Point(247, 379);
+            this.lblDelta.Name = "lblDelta";
+            this.lblDelta.Size = new System.Drawing.Size(13, 13);
+            this.lblDelta.TabIndex = 24;
+            this.lblDelta.Text = "--";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(548, 501);
+            this.Controls.Add(this.lblDelta);
+            this.Controls.Add(this.lblEyx);
+            this.Controls.Add(this.stringToWriteTextBox);
+            this.Controls.Add(this.stringReadTextBox);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.stringReadLabel);
+            this.Controls.Add(this.readButton);
+            this.Controls.Add(this.stringToWriteLabel);
+            this.Controls.Add(this.writeButton);
+            this.Controls.Add(this.closeButton);
+            this.Controls.Add(this.openButton);
+            this.Controls.Add(this.secondaryAddressComboBox);
+            this.Controls.Add(this.primaryAddressNumericUpDown);
+            this.Controls.Add(this.boardIdNumericUpDown);
+            this.Controls.Add(this.secondaryAddressLabel);
+            this.Controls.Add(this.primaryAddressLabel);
+            this.Controls.Add(this.boardIdLabel);
+            this.Name = "MainForm";
+            this.Text = "Main";
+            ((System.ComponentModel.ISupportInitialize)(this.primaryAddressNumericUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.boardIdNumericUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label boardIdLabel;
+        private System.Windows.Forms.Label primaryAddressLabel;
+        private System.Windows.Forms.Label secondaryAddressLabel;
+        private System.Windows.Forms.ComboBox secondaryAddressComboBox;
+        private System.Windows.Forms.NumericUpDown primaryAddressNumericUpDown;
+        private System.Windows.Forms.NumericUpDown boardIdNumericUpDown;
+        private System.Windows.Forms.Button closeButton;
+        private System.Windows.Forms.Button openButton;
+        private System.Windows.Forms.Label stringReadLabel;
+        private System.Windows.Forms.Button readButton;
+        private System.Windows.Forms.Label stringToWriteLabel;
+        private System.Windows.Forms.Button writeButton;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.RichTextBox stringReadTextBox;
+        private System.Windows.Forms.TextBox stringToWriteTextBox;
+        private System.Windows.Forms.Label lblEyx;
+        private System.Windows.Forms.Label lblDelta;
+    }
+}
+
---
+++ PolarizationAnalyzer/Form1.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using NationalInstruments.NI4882;
+
+namespace PolarizationAnalyzer
+{
+    public partial class MainForm : Form
+    {
+        private string text_S0 = "VAL00 annnn.nnn;VAL01 annnn.nnn;VAL02 annnn.nnn;VAL03 annnn.nnn;VAL04 annnn.nnn;VAL05 annnn.nnn;VAL06 annnn.nnn;VAL07 annnn.nnn;VAL08 annnn.nnn;VAL09 annnn.nnn;VAL10 annnn.nnn;VAL11 annnn.nnn;VAL12 annnn.nnn;VAL13 annnn.nnn;VAL14 annnn.nnn;nnnn;Enn;";
+        private string[] lables_S0 =
+        {
+            "elevation angle θ",
+            "ellipticity angle η",
+            "axis ratio tan | η |",
+            "degree of polarization DOP",
+            "tan ψ",
+            "power ratio a",
+            "phase difference ∆",
+            "polarized power Ppol",
+            "extinction ratio ER",
+            "normalized Stokes parameter S1",
+            "normalized Stokes parameter S2",
+            "normalized Stokes parameter S3",
+            "polarized power (dBm)",
+            "total power",
+            "unpolarized power(dBm)",
+            "device status code",
+            "error code"
+        };
+
+        private string[] S0(string data)
+        {
+            string[] VAL = data.Split(';');
+
+            string[] values = new string[17];
+
+            for (int i = 0; i < 15; i++)
+            {
+                values[i] = VAL[i].Split(' ')[1];
+            }
+
+            values[15] = VAL[VAL.Length - 3];
+            values[16] = VAL[VAL.Length - 2];
+
+            return values;
+        }
+
+        private void InitsecondaryAddressComboBox()
+        {
+            secondaryAddressComboBox.Items.Add("None");
+            for (int i = 96; i <= 126; i++)
+            {
+                secondaryAddressComboBox.Items.Add(i);
+            }
+            secondaryAddressComboBox.SelectedIndex = 0;
+        }
+
+        private void SetupControlState(bool isSessionOpen)
+        {
+            boardIdNumericUpDown.Enabled = !isSessionOpen;
+            primaryAddressNumericUpDown.Enabled = !isSessionOpen;
+            secondaryAddressComboBox.Enabled = !isSessionOpen;
+            openButton.Enabled = !isSessionOpen;
+            closeButton.Enabled = isSessionOpen;
+            stringToWriteTextBox.Enabled = isSessionOpen;
+            writeButton.Enabled = isSessionOpen;
+            readButton.Enabled = isSessionOpen;
+            stringReadTextBox.Enabled = isSessionOpen;
+        }
+
+        private string ReplaceCommonEscapeSequences(string s)
+        {
+            return s.Replace("\\n", "\n").Replace("\\r", "\r");
+        }
+
+        private string InsertCommonEscapeSequences(string s)
+        {
+            return s.Replace("\n", "\\n").Replace("\r", "\\r");
+        }
+
+        Bitmap CreatBitMap(Color color, int rows, int cols)
+        {
+            Bitmap frame = new Bitmap(cols, rows);
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    frame.SetPixel(i, j, color);
+                }
+            }
+            return frame;
+        }
+
+        void DrowLinesOnBitmap(ref Bitmap frame, Point[] points, Color color)
+        {
+            Graphics g = Graphics.FromImage(frame);
+            Pen pen = new Pen(color);
+            g.DrawLines(pen, points);
+            pen.Dispose();
+            g.Dispose();
+        }
+
+        private void PolarizationEllipse(ref Point[] points, double Eyx, double delata_x)
+        {
+            delta = Math.PI * delata_x;//-PI and +PI
+
+            t = 0;
+
+            for (int i = 0; i < noPoints; i++)
+            {
+                t = (double)i / (noPoints - 1);
+                points[i].X = (int)(Math.Round((Math.Sin(Omega * t) + 1) * 100)) + 1;
+                points[i].Y = (int)(Math.Round((Eyx * Math.Sin((Omega * t) + delta) + 1) * 100)) + 1;
+            }
+        }
+
+        private void DrawCall(ref Bitmap frame, ref Point[] points)
+        {
+            frame = CreatBitMap(Color.White, pictureBox.Width, pictureBox.Height);
+            DrowLinesOnBitmap(ref frame, points, Color.Black);
+            frame.RotateFlip(RotateFlipType.Rotate180FlipX);
+        }
+
+        private Bitmap frame;
+
+        private double Omega;
+        private double Eyx;
+        private double delata_x;
+        private double delta;
+
+        private double t;
+        private Point[] points;
+
+        private int noPoints;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            SetupControlState(false);
+            InitsecondaryAddressComboBox();
+
+            string[] data = S0(text_S0);
+            
+            for (int i = 0; i < 17; i++)
+            {
+                stringReadTextBox.Text += (lables_S0[i] + " - " + data[i] + Environment.NewLine);
+            }
+
+            noPoints = 20;
+            points = new Point[noPoints];
+            Omega = 2 * Math.PI * 1;
+        }
+
+        private void OpenButton_Click(object sender, System.EventArgs e)
+        {
+            try
+            {
+                Cursor.Current = Cursors.WaitCursor;
+                // Convert the Secondary Address Combo Text into a number.
+                int currentSecondaryAddress = 0;
+                if (secondaryAddressComboBox.SelectedIndex != 0)
+                {
+                    currentSecondaryAddress = (int)secondaryAddressComboBox.SelectedItem;
+                }
+                // Intialize the device
+                device = new Device((int)boardIdNumericUpDown.Value, (byte)primaryAddressNumericUpDown.Value, (byte)currentSecondaryAddress);
+                SetupControlState(true);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                Cursor.Current = Cursors.Default;
+            }
+        }
+
+        private void CloseButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                device.Dispose();
+                SetupControlState(false);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void WriteButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                device.Write(ReplaceCommonEscapeSequences(stringToWriteTextBox.Text));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void ReadButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Cursor.Current = Cursors.WaitCursor;
+                stringReadTextBox.Text = InsertCommonEscapeSequences(device.ReadString());
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                Cursor.Current = Cursors.Default;
+            }
+        }
+
+        private void TrackBar1_Scroll(object sender, EventArgs e)
+        {
+            Eyx = (double)trackBar1.Value / 100;//0 - 1
+            lblEyx.Text = Eyx.ToString();
+
+            delata_x = ((double)trackBar2.Value / 100) * 2 - 1;//-1 and 1
+
+
+            PolarizationEllipse(ref points, Eyx, delata_x);
+            DrawCall(ref frame, ref points);
+            pictureBox.Image = frame;
+        }
+
+        private void TrackBar2_Scroll(object sender, EventArgs e)
+        {
+            Eyx = (double)trackBar1.Value / 100;//0 - 1
+
+            delata_x = ((double)trackBar2.Value / 100) * 2 - 1;//-1 and 1
+            lblDelta.Text = delata_x.ToString() + " Pi";
+
+            PolarizationEllipse(ref points, Eyx, delata_x);
+            DrawCall(ref frame, ref points);
+            pictureBox.Image = frame;
+        }
+    }
+}

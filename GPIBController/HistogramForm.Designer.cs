d6e76cae0e3c8cbe41d0cc3fef4f2789dc79147d
kavinda ravishan <kvnravishan@outlook.com>
2020-12-12 23:41:23 +0530

    hist form win size.

--- GPIBController/HistogramForm.Designer.cs
+++ GPIBController/HistogramForm.Designer.cs
@@ -30,9 +30,9 @@ namespace GPIBController
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(HistogramForm));
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.picCloseButton = new System.Windows.Forms.PictureBox();
             this.label23 = new System.Windows.Forms.Label();
             this.txtBoxChartStepSize = new System.Windows.Forms.TextBox();
@@ -80,18 +80,18 @@ namespace GPIBController
             // 
             // chart
             // 
-            chartArea2.Name = "ChartArea1";
-            this.chart.ChartAreas.Add(chartArea2);
-            legend2.Enabled = false;
-            legend2.Name = "Legend1";
-            this.chart.Legends.Add(legend2);
+            chartArea1.Name = "ChartArea1";
+            this.chart.ChartAreas.Add(chartArea1);
+            legend1.Enabled = false;
+            legend1.Name = "Legend1";
+            this.chart.Legends.Add(legend1);
             this.chart.Location = new System.Drawing.Point(12, 42);
             this.chart.Name = "chart";
-            series2.ChartArea = "ChartArea1";
-            series2.IsXValueIndexed = true;
-            series2.Legend = "Legend1";
-            series2.Name = "Data";
-            this.chart.Series.Add(series2);
+            series1.ChartArea = "ChartArea1";
+            series1.IsXValueIndexed = true;
+            series1.Legend = "Legend1";
+            series1.Name = "Data";
+            this.chart.Series.Add(series1);
             this.chart.Size = new System.Drawing.Size(776, 350);
             this.chart.TabIndex = 126;
             this.chart.Text = "chart";
@@ -169,6 +169,8 @@ namespace GPIBController
             this.Controls.Add(this.chart);
             this.Controls.Add(this.picCloseButton);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximumSize = new System.Drawing.Size(800, 450);
+            this.MinimumSize = new System.Drawing.Size(800, 450);
             this.Name = "HistogramForm";
             this.Text = "Histogram";
             ((System.ComponentModel.ISupportInitialize)(this.picCloseButton)).EndInit();
